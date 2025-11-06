8ee81b15c44ad1f6b8dedec0d3e70f0d66da3881
Hari Karthigasu <khprasaad@gmail.com>
2025-11-06 20:53:54 +0100

    PAC-0: Refactoring (#11)
    * Refactored one more time :D
    
    * Refactored one more time :D

---
+++ cdk/cdk/s3_stack.py
@@ -0,0 +1,20 @@
+from aws_cdk import (
+    Stack,
+    aws_s3 as s3,
+    Tags,
+)
+from constructs import Construct
+
+class S3Stack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        Tags.of(self).add("AWSCDK", "True")
+
+        tofu_state = s3.Bucket(self, "s3",
+            bucket_name=f"{construct_id}",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            versioned=True
+        )
+

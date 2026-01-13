176772f9eb0d451ea1ea3640a439a6226dd11a9e
harik8 <khprasaad@gmail.com>
2026-01-13 21:50:54 +0100

    awsing: Minor fixes and new bucket for tofu state

--- cdk/app.py
+++ cdk/app.py
@@ -2,48 +2,20 @@
 import os
 import aws_cdk as cdk
 
-from cdk.test_stack import TestStack
 from cdk.s3_stack import S3Stack
-# from cdk.vpc_stack import VpcStack
-# from cdk.eks_stack import EksStack
-# from cdk.rds_stack import RdsStack
-# from cdk.lambda_stack import LambdaStack
+from dotenv import load_dotenv
+import os
 
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# aws_account_id   = os.getenv('AWS_ACCOUNT_ID')
-# aws_region       = os.getenv('AWS_REGION')
-# aws_account_name = os.getenv('AWS_ACCOUNT_NAME')
+load_dotenv()
 
 app = cdk.App()
-TestStack(app, "h-cdk-test",
-  env=cdk.Environment(account='****', region='***'),
-)
 
 S3Stack(app, "h-tofu-state",
-  env=cdk.Environment(account='****', region='****'),
+  env=cdk.Environment(account=os.getenv('AWS_ACCOUNT_ID'), region=os.getenv('AWS_REGION')),
 )
 
-# vpc = VpcStack(app, f"{aws_account_name}-vpc",
-#   env=cdk.Environment(account=aws_account_id, region=aws_region)
-# )
-
-# eks = EksStack(app, f"{aws_account_name}-eks",
-#   vpc=vpc,
-#   account=aws_account_id,
-#   env=cdk.Environment(account=aws_account_id, region=aws_region)
-# )
-
-# rds = RdsStack(app, f"{aws_account_name}-rds",
-#   vpc=vpc,
-#   account=aws_account_id,
-#   env=cdk.Environment(account=aws_account_id, region=aws_region)
-# )
-
-# _lambda = LambdaStack(app, f"{aws_account_name}-lambda",
-#   env=cdk.Environment(account=aws_account_id, region=aws_region)
-# )
+S3Stack(app, f"{os.getenv('TOFU_WORKSPACE')}-tofu-state",
+  env=cdk.Environment(account=os.getenv('AWS_ACCOUNT_ID'), region=os.getenv('AWS_REGION')),
+)
 
 app.synth()
